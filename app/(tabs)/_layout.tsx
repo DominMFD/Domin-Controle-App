7b6c57214f8d6540c9766b00cbb14a87227d6505
DominMFD <mfdmatheus@hotmail.com>
2025-05-25 22:07:54 -0300

    chore: add icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,9 @@
 import { Tabs } from "expo-router";
 import React from "react";
 import { Platform } from "react-native";
-
-import { HapticTab } from "@/components/HapticTab";
-import { IconSymbol } from "@/components/ui/IconSymbol";
-import TabBarBackground from "@/components/ui/TabBarBackground";
 import { Colors } from "@/constants/Colors";
 import { useColorScheme } from "@/hooks/useColorScheme";
+import { PapperIcon } from "@/assets/images/icons/PapperIcon";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -16,8 +13,6 @@ export default function TabLayout() {
       screenOptions={{
         tabBarActiveTintColor: Colors[colorScheme ?? "light"].tint,
         headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
@@ -32,7 +27,7 @@ export default function TabLayout() {
         options={{
           title: "Exames",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="house.fill" color={color} />
+            <PapperIcon width={24} height={24} color="#000" />
           ),
         }}
       />
@@ -41,7 +36,7 @@ export default function TabLayout() {
         options={{
           title: "Oxigenação",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <PapperIcon width={24} height={24} color="#000" />
           ),
         }}
       />
@@ -50,7 +45,7 @@ export default function TabLayout() {
         options={{
           title: "Remédios",
           tabBarIcon: ({ color }) => (
-            <IconSymbol size={28} name="paperplane.fill" color={color} />
+            <PapperIcon width={24} height={24} color="#000" />
           ),
         }}
       />
