6deb594fec676d085c5f14d93ac1f02f33ea4c67
DominMFD <mfdmatheus@hotmail.com>
2025-07-06 18:25:17 -0300

    chore: exams screen

--- app/(tabs)/exams.tsx
+++ app/(tabs)/exams.tsx
@@ -4,7 +4,7 @@ import { View, Text } from "react-native";
 export default function Exams() {
   return (
     <ContentTemplate>
-      <Text className="text-2xl text-main_red">Exams</Text>
+      <Exams />
     </ContentTemplate>
   );
 }
---
+++ screens/Exams/ExamsScreen.tsx
@@ -0,0 +1,5 @@
+import { View } from "react-native";
+
+export default function ExamsScreen() {
+  return <View></View>;
+}
