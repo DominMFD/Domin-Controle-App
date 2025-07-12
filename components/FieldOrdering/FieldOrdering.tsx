d7a4749cb4c132939fa8eae87b0560cf5c275947
DominMFD <mfdmatheus@hotmail.com>
2025-07-12 14:15:25 -0300

    fix: add key

--- components/FieldOrdering/FieldOrdering.tsx
+++ components/FieldOrdering/FieldOrdering.tsx
@@ -6,7 +6,10 @@ export default function FieldOrdering({ fields }: FieldOrderingProps) {
   return (
     <View className="flex-row bg-main_white rounded-[10px] border border-border_color justify-between">
       {fields.map(value => (
-        <Pressable className="flex-row px-4 py-[11.5px] items-center gap-[8px] flex-1 justify-center border-r border-border_color">
+        <Pressable
+          key={value}
+          className="flex-row px-4 py-[11.5px] items-center gap-[8px] flex-1 justify-center border-r border-border_color"
+        >
           <Text className="text-lg text-main_black font-bold capitalize">
             {value.toLowerCase()}
           </Text>
