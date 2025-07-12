f0029b8a8b662aaf735feb9120706a5c0058e218
DominMFD <mfdmatheus@hotmail.com>
2025-07-12 14:49:01 -0300

    fix: button opacity

--- components/AddButton/AddButton.tsx
+++ components/AddButton/AddButton.tsx
@@ -1,15 +1,16 @@
 import { PlusIcon } from "@/assets/images/icons/PlusIcon";
-import { Pressable } from "react-native";
+import { TouchableOpacity } from "react-native";
 
 export default function AddButton() {
   return (
-    <Pressable
+    <TouchableOpacity
       className="absolute w-[50px] h-[50px] bg-main_white rounded-[10px] border border-border_color justify-center items-center bottom-[30px] right-[30px]"
+      activeOpacity={0.7}
       style={{
         elevation: 5,
       }}
     >
       <PlusIcon width={20} height={20} color="#B22222" />
-    </Pressable>
+    </TouchableOpacity>
   );
 }
